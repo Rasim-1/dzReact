678af0608166836133f96a857c6e660d8f448adf
Rasim-1 <husanbaevrasim315@gmail.com>
2024-10-20 09:56:44 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+  <>
+  <header className="header">
+    <div className="container">
+      <div className="wrapp">
+        <img src="/pngegg.png" alt="" className="header__img" width={40} height={40} />
+        <img className='burger' src="https://static.thenounproject.com/png/708429-200.png" alt=""  width={40} height={40}/>
+      <div className="header__menu">
+        <a href="#">home</a>
+        <a href="#">home</a>
+        <a href="#">home</a>
+        <a href="#">home</a>
+      </div>
+      
+      
+      </div>
+    </div>
+  </header>
+  
+  </>
+  )
+}
+
+export default App
